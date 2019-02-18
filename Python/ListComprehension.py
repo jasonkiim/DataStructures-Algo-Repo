f5ffe9073f410fdad9bb4b39f32ee1365aa5b5c5
jasonkiim <dmdwn2696@naver.com>
2019-02-18 18:41:44 -0500

    Creatd a python specific tip/guides file

---
+++ Python/ListComprehension.py
@@ -0,0 +1,5 @@
+# For now, adding the useful links
+# TODO: Make this a pynb file and make it displayable on GitHub
+
+
+# https://www.pythoncentral.io/how-to-slice-listsarrays-and-tuples-in-python/
