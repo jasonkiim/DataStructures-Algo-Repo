5a71785a478b993f442a98069199702c942145a4
jasonkiim <dmdwn2696@naver.com>
2019-02-28 00:34:51 -0500

    Optimized runtime by 40ms using BFS

--- Leetcode/Min Depth BST.py
+++ Leetcode/Min Depth BST.py
@@ -39,3 +39,47 @@ class Solution(object):
 
 # Need to figure out how to keep t track of the depth at all times and return
 # it at the end rather than placing them in an extra data structure...
+
+
+
+
+
+
+'''
+Leverage the fact that in BFS, we can traverse through the nodes one at a time.
+When we discover that the node does not have left child or right child, then we return the current depth.
+Otherwise, add the depth when we see left/right
+'''
+# We can leverage the fact that in BFS, we're traversing through the nodes one at a time.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+
+        We want to minimize the use of import so just use pop(0) to accomplish BFS
+        """
+
+        # Use BFS
+        queue = []
+        if root is not None:
+            queue.append((1, root))
+        else:
+            return 0
+
+        while queue:
+            c_depth, c_root = queue.pop(0)
+            if not c_root.left and not c_root.right:
+                return c_depth
+            if c_root.left:
+                queue.append((c_depth+1, c_root.left))
+            if c_root.right:
+                queue.append((c_depth+1, c_root.right))
