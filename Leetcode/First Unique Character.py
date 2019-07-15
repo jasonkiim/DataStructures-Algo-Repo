15dce02789613b59c78e8075805fb7282b71098c
jasonkiim <dmdwn2696@naver.com>
2019-07-14 22:58:33 -0400

    Optimize code

--- Leetcode/First Unique Character.py
+++ Leetcode/First Unique Character.py
@@ -7,21 +7,14 @@ class Solution(object):
 
         dic = {}
 
-        # Get a count of each characters from the string
-        for i in range (0, len(s)):
-            if s[i] not in dic:
-                dic[s[i]] = 1
+        for c in s:
+            if c in dic:
+                dic[c] += 1
             else:
-                dic[s[i]] += 1
+                dic[c] = 1
 
-        # Iterate through the string once again and get the
-        # first character with the counter 1
-        for i in range (0, len(s)):
-            if dic[s[i]] is 1:
-                return s.index(s[i])
+        # Because we can't maintain the order in the dictionary, iterate through the string once again...
+        for c in s:
+            if dic[c] == 1:
+                return s.find(c)
         return -1
-
-
-
-
-        
