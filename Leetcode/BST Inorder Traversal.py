0d3ec8f0a702dffaa8cc44a1ce5142de0d9d1711
jasonkiim <dmdwn2696@naver.com>
2019-09-03 21:28:48 +0900

    Added recursive solution

--- Leetcode/BST Inorder Traversal.py
+++ Leetcode/BST Inorder Traversal.py
@@ -5,6 +5,34 @@
 #         self.left = None
 #         self.right = None
 
+# class Solution(object):
+#     def inorderTraversal(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: List[int]
+#         """
+
+        # Leverage the stack (append and pop).
+        # Append to stack until the root -> root.left is none
+        # Move the current to stack.pop() when the stack isn't empty but current is none (AKA no left node to check)
+        # Append the current value to the rType list and move forward to current.right
+        # If the stack and current are both empty, then break out of inifnite loop.
+        # stack = []
+        # rType = []
+        # current = root
+        # while True:
+        #     if current is not None:
+        #         stack.append(current)
+        #         current = current.left
+        #     elif stack:
+        #         current = stack.pop()
+        #         rType.append(current.val)
+        #         current = current.right
+        #     else:
+        #         break
+        # return rType
+
+# We need a helper because when the function gets called recursively, rType gets cleared out.
 class Solution(object):
     def inorderTraversal(self, root):
         """
@@ -12,22 +40,16 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # Leverage the stack (append and pop).
-        # Append to stack until the root -> root.left is none
-        # Move the current to stack.pop() when the stack isn't empty but current is none (AKA no left node to check)
-        # Append the current value to the rType list and move forward to current.right
-        # If the stack and current are both empty, then break out of inifnite loop.
-        stack = []
         rType = []
-        current = root
-        while True:
-            if current is not None:
-                stack.append(current)
-                current = current.left
-            elif stack:
-                current = stack.pop()
-                rType.append(current.val)
-                current = current.right
-            else:
-                break
+        self.helper(root, rType)
         return rType
+
+    def helper(self, root, rType):
+        if root:
+            if root.left:
+                self.helper(root.left, rType)
+            rType.append(root.val)
+            if root.right:
+                self.helper(root.right, rType)
+
+    
