53617aada0050031536160cf0646426745b9c9b6
jasonkiim <dmdwn2696@naver.com>
2019-09-03 21:04:23 +0900

    Solved BST inorder traversal problem

---
+++ Leetcode/BST Inorder Traversal.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        # Leverage the stack (append and pop).
+        # Append to stack until the root -> root.left is none
+        # Move the current to stack.pop() when the stack isn't empty but current is none (AKA no left node to check)
+        # Append the current value to the rType list and move forward to current.right
+        # If the stack and current are both empty, then break out of inifnite loop.
+        stack = []
+        rType = []
+        current = root
+        while True:
+            if current is not None:
+                stack.append(current)
+                current = current.left
+            elif stack:
+                current = stack.pop()
+                rType.append(current.val)
+                current = current.right
+            else:
+                break
+        return rType
