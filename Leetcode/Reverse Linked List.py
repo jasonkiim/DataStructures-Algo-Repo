0bfa4d475502c4904f86840ed64412d72cd078ff
jasonkiim <dmdwn2696@naver.com>
2019-09-30 22:47:07 -0400

    Modify answer to include both iterative and recursive approach

--- Leetcode/Reverse Linked List.py
+++ Leetcode/Reverse Linked List.py
@@ -5,23 +5,38 @@
 #         self.next = None
 
 class Solution(object):
+    # Recursive approach
+    def reverseList1(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+
+        if not head or not head.next:
+            return head
+
+        p = self.reverseList(head.next)
+        # Let's say head is 4. Go from 4-> 5 to 5 -> 4 by setting next of 5 as 4. Then, set head.next as null to     accomplish linked list going backwards.
+        head.next.next = head
+        head.next = None
+
+        return p
+
+    # Iterative approach
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        stack = []
-        current_node = head
-        l1 = ListNode(0)
-        output_head = l1
 
-        while current_node is not None:
-            stack.append(current_node)
-            current_node = current_node.next
+        prev = None
+        current = head
+        while current:
+            actual_next = current.next
+            current.next = prev
+            prev = current
+            current = actual_next
 
-        while stack:
-            node = stack.pop()
-            l1.next = ListNode(node.val)
-            l1 = l1.next
-
-        return output_head.next
+        # We return prev because at the end, the prev is at the end of the original linked list
+        return prev
