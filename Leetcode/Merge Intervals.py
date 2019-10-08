4eae51d70605bebab4c6502332d72a357cdb41d5
jasonkiim <dmdwn2696@naver.com>
2019-10-08 01:28:39 -0400

    Update with better comment

--- Leetcode/Merge Intervals.py
+++ Leetcode/Merge Intervals.py
@@ -9,11 +9,11 @@ class Solution(object):
         r_type = []
         intervals = sorted(intervals, key = lambda x: x[0])
 
-        # Appending to r_type and this becomes the "previous", which should happen if the r_type list is empty or the
-        # End time of the current window does not overlap with the previous end time
+        # Appending to r_type and the last element in the r_type becomes the "previous" interval,
+        # which should be appended if the r_type list is empty or the
+        # start time of the current window does not overlap wit the last element.
         # If the end time of the "previous" time overlaps with the start time of the "current", then we want to find the new end time from current and prev. save as the
         # one that has the greater end time.
-        # Otherwise, simply append
         for i in range(len(intervals)):
             if not r_type or r_type[-1][1] < intervals[i][0]:
                 r_type.append(intervals[i])
